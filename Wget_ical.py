8016230f9ae8cb5b111db5511a8e54cc53e0940d
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-03-20 18:18:24 +0100

    new file:   .vscode/settings.json 	modified:   Wget_ical.py

--- Wget_ical.py
+++ Wget_ical.py
@@ -1,7 +1,7 @@
 import wget  # type: ignore
 
 
-def FetchIcalFile(url):
+def fetch_ical_file(url):
     """
     Fetch .ical file from url
     """
@@ -9,7 +9,7 @@ def FetchIcalFile(url):
 
 
 if __name__ == "__main__":
-    FetchIcalFile('http://192.168.88.206:7878/feed/v3/calendar/Radarr.ics?unmonitored=true&asAllDay=true&apikey=fbb06d91e5cb4905ab37539a68c900c6')
+    fetch_ical_file('http://192.168.88.206:7878/feed/v3/calendar/Radarr.ics?unmonitored=true&asAllDay=true&apikey=fbb06d91e5cb4905ab37539a68c900c6')
 
 # http://192.168.88.206:7878/feed/v3/calendar/Radarr.ics?unmonitored=true&asAllDay=true&apikey=fbb06d91e5cb4905ab37539a68c900c6
 # http://192.168.88.206:30000/feed/v3/calendar/sonarr.ics?pastDays=7&futureDays=31&unmonitored=true&premieresOnly=false&asAllDay=true&apikey=5089fe943413408e895d4584330d7cf9
